c3a3be1db4424725a1b8dd27c3128aba297b8d4a
oktaykurt <oktaykurt2000@gmail.com>
2022-11-20 23:21:26 +0300

    Added files to new branch

--- arima_btc.py
+++ arima_btc.py
@@ -8,10 +8,12 @@ import pandas as pd
 import statsmodels.api as sm
 import numpy as np
 
-data = pd.read_csv('data-sets/BTC-USD-5Y.csv')
-data = data['Adj Close']
+data = pd.read_csv('data-sets/BTC-USD-5Y.csv',index_col='Date')
+data = data[['Adj Close']]
+
+data.index = pd.date_range(start='2017-11-20', end='2022-11-20')
+
 print(data)
-data.index = pd.date_range(start='2018-11-20', end='2022-11-20')
 
 result = adfuller(data)
 print('ADF Statistic: %f' % result[0])
@@ -25,38 +27,32 @@ for key, value in result[4].items():
 
 # %%
 # Original Series
-# fig, (ax1, ax2, ax3) = plt.subplots(3)
-# ax1.plot(data.values)
-# ax1.set_title('Original Series')
-# ax1.axes.xaxis.set_visible(False)
-# # 1st Differencing
-# ax2.plot(data.diff())
-# ax2.set_title('1st Order Differencing')
-# ax2.axes.xaxis.set_visible(False)
-# # 2nd Differencing
-# ax3.plot(data.diff().diff())
-# ax3.set_title('2nd Order Differencing')
+fig, (ax1, ax2, ax3) = plt.subplots(3)
+ax1.plot(data.values)
+ax1.set_title('Original Series')
+ax1.axes.xaxis.set_visible(False)
+# 1st Differencing
+ax2.plot(data.diff())
+ax2.set_title('1st Order Differencing')
+ax2.axes.xaxis.set_visible(False)
+# 2nd Differencing
+ax3.plot(data.diff().diff())
+ax3.set_title('2nd Order Differencing')
 
 # %%
-# plot_pacf(data.diff().dropna())
+plot_pacf(data.diff().dropna())
 
-
-# # %%
-# plot_acf(data.diff().dropna())
 
 # %%
-model = ARIMA(data['2020-11-20':], order=(4, 5, 20))
+plot_acf(data.diff().dropna())
+
+# %%
+model = ARIMA(data['2019-11-20':], order=(1, 1, 1))
 model_fit = model.fit()
 
-# p = 8 sectik cünkü pacf grafiğinde 8 den sonra oldukça fazla değişim görülüyor.
-
-# q = 1 sectik cunku acf grafiginde 1'den sonra oldukca fazla degisim goruluyor.
-
-# d = 1 sectik cünkü veride tren
-
-predicted_data = model_fit.predict(start="2020-11-20", end="2022-11-20")
+predicted_data = model_fit.predict(start="2020-11-20")
 new_data = data['2020-11-20':]
-
+new_data = new_data['Adj Close']
 
 # %%
 error = abs(np.divide(
@@ -65,12 +61,16 @@ error_index = new_data.index
 
 # %%
 error_df = pd.DataFrame(error, error_index)
-print(error_df.values)
-plt.plot(error_df.values)
+plt.plot(error_df, )
+plt.title('Error (%)')
 
 fig, ax = plt.subplots()
 
-ax = data.loc['2019-11-20':].plot(ax=ax, color='r')
-predicted_data.plot(ax=ax, color='g')
+ax = data.loc['2019-11-20':].plot(ax=ax, color='c')
+predicted_data.plot(ax=ax, color='m', title='Prediction')
+plt.title('Actual Data vs Prediction')
+
+plt.legend(('Actual Data', 'Prediction'),
+           loc='upper right')
 
 plt.show()
